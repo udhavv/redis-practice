7e81b6a6fcd4f4260a9188173ac4e086791e656b
udhavv <auddhav074@gmail.com>
2026-08-30 15:03:47 +0545

    fiixing database url

--- prisma7.config.ts
+++ prisma7.config.ts
@@ -3,12 +3,16 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const env= process.env.NODE_ENV
+
+const url= env=== "development" ? process.env.DATABASE_URL : process.env.DATABASE_URL_ONLINE
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: url,
   },
 });
